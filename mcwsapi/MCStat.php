e3e60e9b155355726090e8b21b57b8ed619f175e
Defective4 <def3ctive4@gmail.com>
2023-04-12 14:58:34 +0200

    Exceptions

--- mcwsapi/MCStat.php
+++ mcwsapi/MCStat.php
@@ -1,12 +1,36 @@
 <?php
 
+// MIT License
+
+// Copyright (c) 2023 Defective4
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 namespace mcwsapi;
 
+require_once 'mcwsapi/IOException.php';
 class MCStat {
 
     public static function stat(String $host, int $port) {
         $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-        socket_connect($socket, $host, $port);
+        if (!@socket_connect($socket, $host, $port)) {
+            throw new IOException("Couldn't connect to the server", 0);
+        }
 
         $hs = MCStat::createHandshake(762, $host, $port, 1);
         socket_write($socket, MCStat::createVarInt(strlen($hs)));
@@ -14,9 +38,11 @@ class MCStat {
         socket_write($socket, pack("c", 1));
         socket_write($socket, pack("c", 0));
 
-        MCStat::readVarInt($socket);
-        MCStat::readVarInt($socket); // TODO Error handling
-        
+        if (MCStat::readVarInt($socket) <= 0)
+            throw new IOException("Invalid received packet length", 1);
+        if (MCStat::readVarInt($socket) != 0)
+            throw new IOException("Invalid received packet ID", 2);
+
         $raw = MCStat::readString($socket);
         return json_decode($raw, true, 999, JSON_THROW_ON_ERROR);
     }
